5ba6c44aa55162e3fb56fd83c28b88353ae2c80d
Kannan <55331758+Kannan-KR@users.noreply.github.com>
2022-06-01 12:56:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("./connect");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const registerRouter = require("./router/register");
 const userRouter = require("./router/userRouter");
@@ -9,10 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect();
 
 app.use("/register", registerRouter);
 app.use("/users", userRouter);
 
-app.listen(3001);
+app.listen(process.env.PORT);
